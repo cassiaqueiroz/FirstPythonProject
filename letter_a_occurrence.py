e582e34e466ff166754ce8ca5a684759eb35daa2
Cássia <cassiarsqueiroz@gmail.com>
2023-01-28 03:52:32 -0300

    letter_a_occurrence file creation

---
+++ letter_a_occurrence.py
@@ -0,0 +1,7 @@
+print("Write your name: ")
+name = input()
+print("\nWelcome,", name + "!")
+number_of_letters = len(name)
+print("\nTotal number of letters in your name:", number_of_letters, ";")
+letter_a_occurrence = name.count("a")
+print("Letter 'a' occurrence:", letter_a_occurrence, ".")
